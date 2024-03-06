74e53972445fb04ced70e4dac0e4f0986ef0922f
Soung Oo Lwin <soungoolwin275@gmail.com>
2024-03-06 20:10:41 +0700

    Nav and Hero Section Responsive

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,9 +1,11 @@
 <nav class="bg-[#77BFA3] h-[85px] flex items-center">
-    <div class="grid grid-cols-10 w-[85%] mx-auto text-white">
-        <div class="col-span-2 jost-300 my-auto mx-auto flex">
+    <div class="grid lg:grid-cols-10 grid-cols-12 lg:w-[85%] w-full mx-auto text-white">
+
+        <div class="lg:col-span-2 col-span-6 jost-300 my-auto mx-auto flex">
             <p class="text-2xl">PHARMACY</p><span class="pt-2">.com</span>
         </div>
-        <div class="col-span-4 my-auto">
+
+        <div class="col-span-4 my-auto hidden lg:block">
             <div class='max-w-md mx-auto'>
                 <div
                     class="relative flex items-center w-full h-12 rounded-lg focus-within:shadow-lg bg-white overflow-hidden">
@@ -30,47 +32,182 @@
                 </div>
             </div>
         </div>
-        <div class="col-span-4 my-auto flex mx-auto">
-            <div class="flex mr-5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
-                </svg>
 
-                <p>Upload</p>
-            </div>
-            <div class="flex mr-5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
-                </svg>
+        <div class="col-span-4 hidden lg:flex">
+            <div class="my-auto flex mx-auto">
+                <div class="flex mr-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+                    </svg>
 
-
-                <p>Offers</p>
-            </div>
-            <div class="flex mr-5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
-                </svg>
+                    <p>Upload</p>
+                </div>
+                <div class="flex mr-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+                    </svg>
 
 
-                <p>Cart</p>
-            </div>
-            <div class="flex mr-5">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
+                    <p>Offers</p>
+                </div>
+                <div class="flex mr-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                    </svg>
 
 
-                <p>Signin / Signup</p>
+                    <p>Cart</p>
+                </div>
+                <div class="flex mr-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6 mr-1">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                    </svg>
+
+
+                    <p>Signin</p>
+                </div>
             </div>
         </div>
+
+        <div class="lg:hidden col-span-6 flex justify-end my-auto mx-auto">
+            <button id="menu-toggle" class="text-white hover:text-white focus:outline-none focus:text-white">
+                <svg viewBox="0 0 20 20" fill="currentColor" class="menu w-6 h-6">
+                    <path fill-rule="evenodd"
+                        d="M2 5a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm0 6a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1zm0 6a1 1 0 0 1 1-1h14a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </button>
+        </div>
+
     </div>
 </nav>
+
+{{-- mobile nav --}}
+
+<div id="mobile-menu" class="lg:hidden hidden jost-300 bg-gray-100">
+    <div class="px-4 py-2 flex items-center flex-col">
+        <a href="#" class="py-2 px-4 flex hover:text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+            </svg>
+
+            <p>Upload</p>
+        </a>
+        <a href="#" class="flex py-2 px-4 hover:text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M9.568 3H5.25A2.25 2.25 0 0 0 3 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 0 0 5.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 0 0 9.568 3Z" />
+                <path stroke-linecap="round" stroke-linejoin="round" d="M6 6h.008v.008H6V6Z" />
+            </svg>
+
+
+            <p>Offers</p>
+        </a>
+        <a href="#" class="flex py-2 px-4 hover:text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+            </svg>
+
+
+            <p>Cart</p>
+        </a>
+
+        <a href="#" class="flex py-2 px-4 hover:text-gray-700">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                stroke="currentColor" class="w-6 h-6 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round"
+                    d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+
+
+            <p>Signin</p>
+        </a>
+        <div class="relative inline-block text-left">
+            <button type="button"
+                class="inline-flex justify-center w-full  px-4 py-2 text-sm font-medium text-gray-700 active:bg-gray-200 active:text-gray-800"
+                id="dropdown-button" aria-haspopup="true" aria-expanded="true">
+                <!-- Heroicon name: chevron-down -->
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="-ml-1 mr-2 h-5 w-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+                Options
+
+            </button>
+
+            <div class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none hidden"
+                role="menu" aria-orientation="vertical" aria-labelledby="dropdown-button" tabindex="-1"
+                id="dropdown-menu">
+                <div class="py-1" role="none">
+                    <!-- Dropdown items -->
+                    <a href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        role="menuitem" tabindex="-1" id="dropdown-item-1">Item 1</a>
+                    <a href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        role="menuitem" tabindex="-1" id="dropdown-item-2">Item 2</a>
+                    <a href="#"
+                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                        role="menuitem" tabindex="-1" id="dropdown-item-3">Item 3</a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+
+
+<div class="lg:flex hidden justify-center mt-2">
+    <a href="" class="mr-7">
+        <p class="jost-300">Covid Essentials</p>
+    </a>
+    <a href="" class="mr-7">
+        <p class="jost-300">Diabetes</p>
+    </a>
+    <a href="" class="mr-7">
+        <p class="jost-300">Fitness</p>
+    </a>
+    <a href="" class="mr-7">
+        <p class="jost-300">Mom and Baby</p>
+    </a>
+    <a href="" class="mr-7">
+        <p class="jost-300">Treatments</p>
+    </a>
+    <a href="" class="mr-7">
+        <p class="jost-300">Beauty</p>
+    </a>
+</div>
+
+
+
+<script>
+    const menuToggle = document.getElementById('menu-toggle');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    menuToggle.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+
+
+    const dropdownButton = document.getElementById('dropdown-button');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    dropdownButton.addEventListener('click', () => {
+        dropdownMenu.classList.toggle('hidden');
+    });
+</script>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
 <x-layout>
-    <div class="grid grid-cols-12 mx-auto h-[25rem]">
+    <div class="grid grid-cols-12 mx-auto lg:h-[25rem] h-[20rem] mt-7">
         <div class="col-span-6 pl-5 my-auto mx-auto">
-            <h1 class="text-[60px] jost-500 text-[#3d3c3c] pl-5 leading-none">Your Trusted <br> Pharmacy</h1><br>
+            <h1 class="lg:text-[60px] text-[20px] jost-500 text-[#3d3c3c] pl-5 leading-none">Your Trusted <br> Pharmacy
+            </h1><br>
             <p class="pl-5 jost-300">We provide a wide range of medicines and healthcare products. Your health is our
                 priority.
             </p>
         </div>
         <div class="col-span-6">
-            <img src="{{ Vite::image('heroimage.png') }}" alt="Hero Section Image" class="object-cover h-[400px] mx-auto">
+            <img src="{{ Vite::image('heroimage.png') }}" alt="Hero Section Image"
+                class="object-scale-down h-[300px] mx-auto">
         </div>
     </div>
 </x-layout>
